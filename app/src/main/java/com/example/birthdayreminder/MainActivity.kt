7269ac99a49f2c71e50d5c2ba4984c054ca4dbb9
emirhn-aydnlk <emirhn.aydnlk@outlook.com>
2026-01-12 20:45:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/birthdayreminder/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.birthdayreminder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val butonEkle = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.fabEkle)
+
+        butonEkle.setOnClickListener {
+            val gecis = android.content.Intent(this, AddBirthdayActivity::class.java)
+
+            startActivity(gecis)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
